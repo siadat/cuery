93e8b9ca98e2869fcef9f8dbc808ae8dd5d72f23
Sina Siadat <siadat@gmail.com>
2024-06-02 01:16:44 +0000

    simple macro for testing

--- src/jq/main.clj
+++ src/jq/main.clj
@@ -22,21 +22,25 @@
       (map? query) (into {} (map (fn [%] [(key %) (jq (val %) item)]) query))
       :else (get-in item [query]))))
 
-(assert (test/is (= "value1" (jq :key1 {:key1 "value1"}))))
-(assert (test/is (= "value1" (jq [:key1] {:key1 "value1"}))))
-(assert (test/is (= "value2" (jq [:key1 :key2] {:key1 {:key2 "value2"}}))))
-(assert (test/is (= ["valueA"] (jq [:key1 [:key2]] {:key1 [{:key2 "valueA"}]}))))
-(assert (test/is (= ["valueA" "valueB"] (jq [:key1 [:key2]] {:key1 [{:key2 "valueA"} {:key2 "valueB"}]}))))
-(assert (test/is (= "value3" (jq [#(:key1 %)] {:key1 "value3"}))))
-(assert (test/is (= {:keyA "value3"} (jq [{:keyA [:key1]}] {:key1 "value3"}))))
-(assert (test/is (= {:keyA "value3" :keyB "value3"} (jq [{:keyA [:key1] :keyB [:key1]}] {:key1 "value3"}))))
-(assert (test/is (= {:key1 "value3" :keyY [{:key4 "valueA"} {:key4 "valueB"}]}
-                    (jq
-                      [{:key1 [:key1]
-                        :keyY [:key3]}]
-                      {:key1 "value3"
-                       :key3 [{:key4 "valueA"}
-                              {:key4 "valueB"}]}))))
+(defmacro assert-eq [expected actual]
+  `(assert (test/is (= ~expected ~actual))))
+
+(assert-eq "value1" (jq :key1 {:key1 "value1"}))
+(assert-eq "value1" (jq [:key1] {:key1 "value1"}))
+(assert-eq "value2" (jq [:key1 :key2] {:key1 {:key2 "value2"}}))
+(assert-eq ["valueA"] (jq [:key1 [:key2]] {:key1 [{:key2 "valueA"}]}))
+(assert-eq ["valueA" "valueB"] (jq [:key1 [:key2]] {:key1 [{:key2 "valueA"} {:key2 "valueB"}]}))
+(assert-eq "value3" (jq [#(:key1 %)] {:key1 "value3"}))
+(assert-eq {:keyA "value3"} (jq [{:keyA [:key1]}] {:key1 "value3"}))
+(assert-eq {:keyA "value3" :keyB "value3"} (jq [{:keyA [:key1] :keyB [:key1]}] {:key1 "value3"}))
+(assert-eq {:key1 "value3" :keyY [{:key4 "valueA"} {:key4 "valueB"}]}
+           (jq
+             [{:key1 [:key1]
+               :keyY [:key3]}]
+             {:key1 "value3"
+              :key3 [{:key4 "valueA"}
+                     {:key4 "valueB"}]}))
+
 ;; Parsing JSON
 (assert (test/is (= 3.14 (json/read-str (:out (proc/sh "echo 3.14"))))))
 
